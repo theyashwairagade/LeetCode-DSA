7b58f77a01c77fe078b62acfbea8ec3935e49487
Yash Wairagade <theyashwairagade@gmail.com>
2024-05-02 23:33:49 +0530

    Solved jump game in C++

---
+++ 55_Jump_game.cpp
@@ -0,0 +1,11 @@
+// Greedy
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+            if(i>j) return false;
+            else    j=max(j,i+nums[i]);
+        return true;
+    }
+};
