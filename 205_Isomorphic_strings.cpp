19fba5ab91fa061cabea9d271aa9187bf2b786bc
Yash Wairagade <theyashwairagade@gmail.com>
2024-05-09 16:37:20 +0530

    Solved isomorphic strings in C++

---
+++ 205_Isomorphic_strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> m1,m2;
+        for(int i=0;i<s.length();i++)
+            if(!m1.count(s[i]) and !m2.count(t[i]))
+                m1[s[i]]=t[i], m2[t[i]]=s[i];
+            else
+                if(m1[s[i]]!=t[i] or m2[t[i]]!=s[i]) 
+                    return false;
+        return true;
+    }
+};
