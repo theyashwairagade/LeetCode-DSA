9faa241fca71dbd9e46baf50d6f093457ab91216
Yash Wairagade <theyashwairagade@gmail.com>
2024-05-20 01:06:11 +0530

    Solved find number of ways to reach the kth stair in C++

---
+++ 3154_Find_number_of_ways_to_reach_the_kth_stair.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    unordered_map<int, vector<vector<int>>> memo;
+
+    int findWays(int targetStep, int currentStep, int jumpPower, bool canJump) {
+        if (currentStep > targetStep + 5)
+            return 0;
+
+        if (jumpPower > 31)
+            return 0;
+
+        if (memo.count(currentStep) && memo[currentStep][jumpPower][canJump] != -1)
+            return memo[currentStep][jumpPower][canJump];
+
+        int result = (currentStep == targetStep);
+
+        if (canJump)
+            result += findWays(targetStep, currentStep - 1, jumpPower, false);
+
+        if (currentStep + (1 << jumpPower) <= targetStep + 1)
+            result += findWays(targetStep, currentStep + (1 << jumpPower), jumpPower + 1, true);
+
+        memo[currentStep].resize(32, vector<int>(2, -1));
+
+        return memo[currentStep][jumpPower][canJump] = result;
+    }
+
+public:
+    int waysToReachStair(int targetStep) {
+        return findWays(targetStep, 1, 0, true);
+    }
+};
