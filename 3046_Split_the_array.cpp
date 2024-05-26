01763bf4233e813158cfa7946cd61883c76352b7
Yash Wairagade <theyashwairagade@gmail.com>
2024-05-27 00:38:16 +0530

    Solved split the array in C++

---
+++ 3046_Split_the_array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        map<int,int> mp;
+        for(int number: nums)   mp[number]++;
+        for(auto pair: mp)  if(mp[pair.first]>2)  return false;
+        return true;
+    }
+};
