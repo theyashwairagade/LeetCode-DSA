c603efdc7ee6deb28ea9a340f523484c8a11c633
Yash Wairagade <theyashwairagade@gmail.com>
2024-06-03 00:09:53 +0530

    Solved find the number of good pairs 1 in C++

---
+++ 3162_Find_the_number_of_good_pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int ans=0;
+        for(auto &i: nums1)
+            for(auto &j: nums2)
+                if(!(i%(j*k)))
+                    ans++;
+        return ans;
+    }
+};
